72605283162af1480e1f569c222d5d8d9cf61b90
santa <santa@kasa.sg>
2023-04-19 19:30:32 +0900

    first commit

---
+++ src/AppJSX.jsx
@@ -0,0 +1,25 @@
+import './App.css';
+
+function AppJSX() {
+  const name = '엘리';
+  const list = ['우유', '딸기', '바나나', '요거트'];
+  return (
+    <>
+      <h1 className='orange'>{`Hello! ${name}`}</h1>
+      <h2>Hello!</h2>
+      <p>{name}</p>
+      <ul>
+        {list.map((item) => (
+          <li>{item}</li>
+        ))}
+      </ul>
+      <img 
+        style={{ width: '200px', height: '200px' }}
+        src='https://images.unsplash.com/photo-1661956603025-8310b2e3036d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80'
+        alt='nature'
+      />
+    </>
+  );
+}
+
+export default AppJSX;
---
+++ src/AppProfile.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import Profile from './components/Profile';
+
+function AppProfile() {
+  return (
+    <>
+      <Profile />
+      <Profile />
+      <Profile />
+    </>
+  );
+}
+
+export default AppProfile;
